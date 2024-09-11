9068255eeeceffc56ab7aa46e340ba1b75a07d0e
Tony <111481792+Bowist27@users.noreply.github.com>
2024-09-10 19:52:33 -0600

    Codigo depurado

--- Operacion.c
+++ Operacion.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+
+
+
 int main(){
 int num1 = 12;
 int num2 = 3;
@@ -9,7 +12,6 @@ printf("%i",op1);
 int op2 = resta(num1,num2);
 13
 printf("%i",op2);
-int op3 = multi(num1,num2);
-printf("%i",op3);
+
 return 0;
 }
